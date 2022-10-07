cdc9b623bd91892f720c0ef86e56d04b36a59b5f
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2022-10-07 19:32:50 +0530

    st 1 dvc done

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -18,10 +18,9 @@ logging.basicConfig(
     )
 
 
-def main(config_path, params_path):
+def main(config_path):
     ## read config files
     config = read_yaml(config_path)
-    params = read_yaml(params_path)
     
     source_data = config["source_data"]
     input_data = os.path.join(source_data["data_dir"], source_data["data_file"])
@@ -46,13 +45,12 @@ def main(config_path, params_path):
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        main(config_path=parsed_args.config)
         logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
