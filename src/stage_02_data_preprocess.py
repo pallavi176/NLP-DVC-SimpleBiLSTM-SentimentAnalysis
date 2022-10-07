136497e30eec15033fbee7ae36be5972f8f47e8c
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2022-10-07 20:22:31 +0530

    st 2 dataset read

---
+++ src/stage_02_data_preprocess.py
@@ -0,0 +1,47 @@
+import os
+import argparse
+import logging
+import pandas as pd
+from src.utils.common import read_yaml, create_directories
+import warnings
+warnings.filterwarnings('ignore')
+warnings.simplefilter('ignore')
+
+
+STAGE = "STAGE 2" 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main(config_path, params_path):
+    ## read config files
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+    
+    artifacts = config["artifacts"]
+    prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
+    dataset_file = os.path.join(prepared_data_dir_path, artifacts["DATASET_FILE"])
+
+    dataset = pd.read_csv(dataset_file)
+    #print(dataset.head())
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
