2537d3b3b114d8cc6444afbebe8a37a8e71ed0a8
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2022-10-08 09:33:35 +0530

    Stage 2 data preprocessing done

--- src/stage_02_data_preprocess.py
+++ src/stage_02_data_preprocess.py
@@ -1,8 +1,11 @@
 import os
 import argparse
 import logging
+import random
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from src.utils.common import read_yaml, create_directories
+from src.utils.data_management import preprocess_df
 import warnings
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
@@ -27,8 +30,33 @@ def main(config_path, params_path):
     prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
     dataset_file = os.path.join(prepared_data_dir_path, artifacts["DATASET_FILE"])
 
+    preprocessed_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPROCESSED_DATA"])
+    create_directories([preprocessed_data_dir_path])
+
+    train_file = os.path.join(preprocessed_data_dir_path, artifacts["TRAIN_DATA"])
+    test_file = os.path.join(preprocessed_data_dir_path, artifacts["TEST_DATA"])
+
+    split = params["preprocess"]["split"]
+    seed = params["preprocess"]["seed"]
+    random.seed(seed)
+
     dataset = pd.read_csv(dataset_file)
-    #print(dataset.head())
+    df = preprocess_df(dataset)
+    print(df.head())
+
+    X = df[['text']]
+    y = df['label']
+
+    # Train Test Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split, random_state=seed)
+
+    print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+
+    train_ds = pd.concat([X_train, pd.DataFrame(y_train)], axis=1)
+    test_ds = pd.concat([X_test, pd.DataFrame(y_test)], axis=1)
+
+    train_ds.to_csv(train_file, index=False)
+    test_ds.to_csv(test_file, index=False)
 
 
 if __name__ == '__main__':
---
+++ src/utils/data_management.py
@@ -0,0 +1,79 @@
+import re
+import string
+import nltk
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
+def remove_tags(text):
+    p = re.compile(r'<.*?>')
+    return p.sub('', text)
+
+def remove_url(txt):
+    return re.sub(r'\s*https?://\S+(\s+|$)', '', txt, flags=re.MULTILINE)
+
+def remove_hashtag(txt):
+    return re.sub(r'@[A-Za-z0-9]+', '', txt, flags=re.MULTILINE)
+
+def remove_punc(txt):
+    return txt.translate(str.maketrans('', '', string.punctuation))
+
+def remove_spchar(text):
+    return re.sub('\W+',' ', text)
+
+def remove_nonascii(txt):
+    encoded_string = txt.encode("ascii", "ignore")
+    return encoded_string.decode()
+
+def remove_stopwords(txt):
+    data = txt.split()
+    new = [word for word in data if not word in stopwords.words('english')]
+    return " ".join(new)
+
+def lematize(text):
+    wnl = WordNetLemmatizer()
+    return " ".join([wnl.lemmatize(i,pos='v') for i in text.split()])
+
+def preprocess_df(df):
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+    nltk.download('omw-1.4')
+
+    print("Downloaded all 3 libraries")
+
+    # Lower all caps
+    df['text'] = df['text'].apply(lambda x: x.lower())
+
+    # Remove Html Tags
+    df['text'] = df['text'].apply(lambda x: remove_tags(x))
+
+    # Remove links:
+    df['text'] = df['text'].apply(lambda x: remove_url(x))
+
+    # Remove hash tag:
+    df['text'] = df['text'].apply(lambda x: remove_hashtag(x))
+
+    # Remove punctuation
+    df['text'] = df['text'].apply(lambda x: remove_punc(x))
+
+    # Remove Special Char
+    df['text'] = df['text'].apply(lambda x: remove_spchar(x))
+
+    #Remove Non- ascii characters
+    df['text'] = df['text'].apply(lambda x: remove_nonascii(x))
+
+    print("Starting stopwords")
+
+    #Remove Stopwords
+    df['text'] = df['text'].apply(lambda x: remove_stopwords(x))
+
+    print("Stopwords done. Starting lemmatization")
+    
+    # Applying lemmatization
+    df['review'] = df['review'].apply(lambda x: lematize(x))
+
+    print("Preprocessing completed")
+
+    return df
+
